18df8112453ab58bef1cf2c2a95ad23264322d5a
dyacob <yacob@geez.org>
2022-11-12 07:56:39 -0500

    Documentation refinement.

--- release/gff/gff_geez_mesob/source/help/gff_geez_mesob.php
+++ release/gff/gff_geez_mesob/source/help/gff_geez_mesob.php
@@ -32,13 +32,10 @@ code {
 
 <br/>
 <a href="#Overview">Overview</a><br/>
-<a href="#Using">Using this Keyboard</a><br/>
-<a style='margin-left:20px' href="#osk">Keyboard Layout</a><br/>
-<a style='margin-left:20px' href="#Quickstart">Quickstart</a><br/>
-<a style='margin-left:20px' href="#Examples">Examples</a><br/>
+<a style='margin-left:20px' href="#Using">Using this Keyboard</a><br/>
+<a style='margin-left:40px' href="#MobileLayout">Mobile Layout &amp; Layers</a><br/>
+<a style='margin-left:40px' href="#TabletLayout">Tablet Layout &amp; Layers</a><br/>
 <a style='margin-left:20px' href="#Documentation">Keyboard Details</a><br/>
-<a style='margin-left:30px' target="_blank" href='AmharicTyping-English.pdf'>Complete Typing Chart - English</a><br/>
-<a style='margin-left:30px' target="_blank" href='AmharicTyping-Amharic.pdf'>Complete Typing Chart - Amharic</a><br/>
 <a href="#Troubleshooting">Troubleshooting</a><br/>
 <a href="#Resources">Further Resources</a><br/>
 <a style='margin-left:20px' href="#Related">Related Keyboard Layouts</a><br/>
@@ -56,19 +53,19 @@ This is a language neutral touch (only) keyboard for mobile devices. The keyboar
 <div id='Using'>
 <h2>Using this Keyboard</h2>
 
-<div>
-<h2><a id="letters" name="mobile"></a>Mobile Layout &amp; Layers</h2>
+<div id="MobileLayout">
+<h3>Mobile Layout &amp; Layers</h3>
 
 <p style="text-align: justify;">
 The mobile keyboard uses the “Mesob” style layout of letters where letters are arranged in a series of 4 rings.  Modifier keys that will change the vowel of a letter appear in the outer most ring. 
 </p>
 
 <p style="text-align: justify;">
-The Mesob layout also has the goal of providing <em>all</em> letters actively used by the languages written with Geʾez script in a single keyboard “layer”. Given the high number of letters appearing on the keyboard, it is a better fit for tablet devices than for typical mobile phones. Consider using the more compact <a href="https://github.com/keymanapp/keyboards/tree/master/release/gff/gff_harege_fidelat">Harege Fidelat Keyboard</a> which is a similar design and fits more comfortably on the smaller mobile phone screens.
+The Mesob layout also has the goal of providing <em>all</em> letters actively used by the languages written with Geʾez script in a single keyboard “layer”. Given the high number of letters appearing on the keyboard, it is a better fit for tablet devices than for typical mobile phones. Consider using the more compact <a href="https://keyman.com/keyboards/gff_harege_fidelat">Harege Fidelat Keyboard</a> which is a similar design and fits more comfortably on the smaller mobile phone screens.
 </p>
 
 <div style="margin-left: 1em;">
-<h3>Letters</h3>
+<h4>Letters</h4>
 <p style="text-align: justify;">
 The most frequently used letters generally appear in the inner rings, and less used letter appear in the out rings. Letter appear in a semi-alphabetic arrangement in the clockwise direction, but not strictly so.  Some effort is made to group related letters together (related either by sound or shape).
 </p>
@@ -101,7 +98,7 @@ Like the letter keys, the <code>።</code> and <code>’</code> keys also have p
 </div>
 
 <div style="margin-left: 1em;">
-<h3>Numerals</h3>
+<h4>Numerals</h4>
 
 <p style="text-align: justify;">
 Tap the `1/፩` key to shift to the numerals layer. The numerals layer presents the Western numbers, Geʾez numbers, and all Geʾez punctuation.
@@ -116,7 +113,7 @@ Tapping the `@` shifts to a 3rd layer to access all remaining punctuation:
 </div>
 
 <div style="margin-left: 1em;">
-<h3>Punctuation</h3>
+<h4>Punctuation</h4>
 
 <p style="text-align: justify;">
 Punctuation is distributed over two layers on mobile phones, and appears on a single layer on tablets where more screen space is available. The first punctuation layer provides the most frequently used set of symbols:
@@ -136,7 +133,7 @@ Simply tap the `ሀለሐ` key to return to the Mesob letters layers.
 </div>
 </div>
 
-<div>
+<div id="TabletLayout">
 <h2>Tablet Layout &amp; Layers</h2>
 
 <p style="text-align: justify;">
@@ -163,14 +160,15 @@ their products.</p>
 <div id="Related">
 <h3>Related Keyboard Layouts</h3>
 <p>Keyboard Layouts for other Ethiopic languages:</p>
-<p><a href="/go?language=ethiopic&amp;keyboard=Keyboard_gff_awn_7">Awngi</a><br/>
+<p><p><a href="/go?language=ethiopic&amp;keyboard=Keyboard_gff_amharic">Amharic</a><br/>
+<a href="/go?language=ethiopic&amp;keyboard=Keyboard_gff_awn_7">Awngi</a><br/>
 <a href="/go?language=ethiopic&amp;keyboard=Keyboard_gff_bcq_7">Bench</a><br/>
 <a href="/go?language=ethiopic&amp;keyboard=Keyboard_gff_blin">Blin</a><br/>
 <a href="/go?language=ethiopic&amp;keyboard=Keyboard_gff_mdx_7">Dizi</a><br/>
 <a href="/go?language=ethiopic&amp;keyboard=Keyboard_gff_geez">Ge'ez</a><br/>
+<a href="/go?language=ethiopic&amp;keyboard=Keyboard_gff_gurage">Gurage</a><br/>
 <a href="/go?language=ethiopic&amp;keyboard=Keyboard_gff_mym_7">Me'en</a><br/>
 <a href="/go?language=ethiopic&amp;keyboard=Keyboard_gff_muz_7">Mursi</a><br/>
-<a href="/go?language=ethiopic&amp;keyboard=Keyboard_gff_sgw_7">Sebatbeit</a><br/>
 <a href="/go?language=ethiopic&amp;keyboard=Keyboard_gff_suq_7">Suri</a><br/>
 <a href="/go?language=ethiopic&amp;keyboard=Keyboard_gff_tigrinya_eritrea">Tigrigna (Eritrea)</a><br/>
 <a href="/go?language=ethiopic&amp;keyboard=Keyboard_gff_tigrinya_ethiopia">Tigrigna (Ethiopia)</a></p>
@@ -178,28 +176,25 @@ their products.</p>
 </div>
 <div>
 <h3>Other Internet Resources</h3>
-<p><a target="_blank" href="http://keymankeyboards.com/?id=455">Download the Amharic Keyboard for Keyman Desktop</a></p>
-<p><a target="_blank" href="http://www.omniglot.com/writing/amharic.htm">More on the Amharic Writing System at Omniglot.com</a><br/>
-<a target="_blank" href="http://en.wikipedia.org/wiki/Amharic_language">More on the Amharic Language at Wikipedia.org</a><br/></p>
-<p><a target="_blank" href="http://www.omniglot.com/writing/ethiopic.htm">More on the Ge'ez Script at Omniglot.com</a><br/>
-<a target="_blank" href="http://en.wikipedia.org/wiki/Ge'ez_alphabet">More on the Ge'ez Script at Wikipedia.org</a></p>
+<p><a target="_blank" href="http://en.wikipedia.org/wiki/Ge'ez_alphabet">More on the Ge'ez Script at Wikipedia.org</a><br/>
+<a target="_blank" href="http://www.omniglot.com/writing/ethiopic.htm">More on the Ge'ez Script at Omniglot.com</a></p>
 </div>
 </div>
 
 <div id="Technical">
 <h2>Technical Information</h2>
 <h3>System Requirements</h3>
-<p>It is recommended that you use an English QWERTY hardware keyboard with this keyboard.
+<p>Due to the keyboard's width it is ideally suited for tablet devices but is also suitable for larger mobile phones.
 </p>
 <h3>Unicode Version</h3>
-<p>This keyboard complies with Unicode 4.1</p>
+<p>This keyboard complies with Unicode 14</p>
 
 </div>
 
 <div id="VersionHistory">
 <h3>Version History</h3>
 <dl>
-  <dt>Version 1.0 23 Oct 2022</dt>
+  <dt>Version 1.0 12 Nov 2022</dt>
   <dd>Initial release.</dd>
 </dl>
 
@@ -212,7 +207,7 @@ This keyboard was created by the Geʾez Frontier Foundation.  SIL International 
 <div id="Copyright">
 <h3>Copyright and Terms of Use</h3>
 <p>
-The Amharic keyboard layout for Keyman Desktop and KeymanWeb is Copyright 1997-2018 Ge'ez Frontier Foundation and SIL International.  It may be freely distributed and used under the terms of <a href="https://opensource.org/licenses/MIT">The MIT License</a>.
+The Amharic keyboard layout for Keyman Desktop and KeymanWeb is Copyright 2022 Geʾez Frontier Foundation and SIL International.  It may be freely distributed and used under the terms of <a href="https://opensource.org/licenses/MIT">The MIT License</a>.
 </p>
 
 </div>
